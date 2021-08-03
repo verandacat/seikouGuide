6ca152efbfcd6c09a05dfeeb6e8f2ce2d214cb1d
verandacat <verandacats@yahoo.co.jp>
2021-08-03 11:28:23 +0900

    生漢煎記事修正1

---
+++ iphone/shokansen.php
@@ -0,0 +1,310 @@
+<?php include('header.php'); ?>
+<link rel="stylesheet" href="kiji.css">
+
+<!-- Memo -->
+<!-- メインカラー #ff4566 #fb468b -->
+
+<div id="wrapper">
+
+    <h1 class="midashi-05">
+        本気でお腹の脂肪を落とすなら効果効能が認められた漢方薬『生漢煎「防風通聖散」』できまり！<br>定期コースで初回50％OFF＋送料無料！＋このページから購入で1,100円OFFクーポン付き！
+    </h1>
+
+    <section id="kiji__box">
+
+        <div class="kiji__fv">
+            <img src="img/kiji/fv-sp.jpg" alt="" width="100%">
+        </div>
+
+        <div class="button">
+            <p class="center font13 bold">＼定期コースなら初回限定50％OFF＋送料無料／</p>
+            <a href="./links/bouhu.php<?php echo "?" . $query2; ?>" target="_blank">公式サイトでお得に購入する<i
+                    class="fas fa-arrow-circle-right"></i></a>
+        </div>
+
+        <!-- 絞り込み 必要に応じてコメント消してください-->
+        <!--<div style="padding:20px 0 0;">
+            <?php //get_search_form(); ?>
+        </div>-->
+
+        <h2 class="midashi-h2-03">こんなことで悩んでいませんか？</h2>
+       
+
+        <div class="text-box">
+            <div class="boxDeco borderBox-pink3 yellowBox">
+                <ul class="kiji__list">
+                    <li>お腹の脂肪が落ちにくくなった</li>
+                    <li>便秘がちでお腹が張ってしまう</li>
+                    <li>むくみが取れなくて太って見える</li>
+                    <li>いろいろなサプリを試したけど効果を感じない</li>
+                    <li>信頼できる医薬品を試したい</li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="text-box">
+            <p>一つでも当てはまったら『生漢煎「防風通聖散」』をお試しください。</p>
+
+            <p class="center"><span class="kpink font24">↓↓↓↓↓↓</span></p>
+
+            <img src="img/kiji/image3.jpg" alt="">
+
+            <p><span class="rred">手ごわいお腹の脂肪も撃退！<br>『生漢煎「防風通聖散」』は効果効能が認められた”医薬品”！</span></p>
+        </div>
+
+        <div class="button">
+            <p class="center font13 bold">＼定期コースなら初回限定50％OFF＋送料無料／</p>
+            <a href="./links/bouhu.php<?php echo "?" . $query2; ?>" target="_blank">公式サイトでお得に購入する<i
+                    class="fas fa-arrow-circle-right"></i></a>
+        </div>
+
+            <h2 class="midashi-h2-03">『生漢煎「防風通聖散」』とは？</h2>
+ 
+
+        <div class="text-box">
+            <img src="img/kiji/image9.png" alt="">
+            <p>『生漢煎「防風通聖散」』は、</p>
+            <p>累計販売数4,500万包を突破した今話題の漢方薬です♪</p>
+            <p>そして、『生漢煎「防風通聖散」』は、</p>
+            <p>❌　サプリメント<br />
+            ⭕　漢方薬（医薬品）</p>
+            <p>です。</p>
+            <p>サプリメントなどの健康食品や機能性表示食品ではなく、<br>
+                <span class="rred">効果効能が認められた「漢方薬（医薬品）」</span>だから<br>
+                <span class="bold">今まで諦めていた</span><span class="rred">おなかの脂肪にも効果を発揮</span>してくれるんです！
+            </p>
+        </div>
+
+        <div class="button">
+            <p class="center font13 bold">＼定期コースなら初回限定50％OFF＋送料無料／</p>
+            <a href="./links/bouhu.php<?php echo "?" . $query2; ?>" target="_blank">公式サイトでお得に購入する<i
+                    class="fas fa-arrow-circle-right"></i></a>
+        </div>
+
+        <h2 class="midashi-h2-03">『生漢煎「防風通聖散」』の５つのおすすめポイント</h2>
+      
+
+        <div class="text-box">
+            <div class="boxDeco borderBox-pink3 yellowBox">
+                <h4 class="komidashi">～５つのおすすめポイント～</h4>
+                <ol class="kiji__list-num">
+                    <li>脂肪を燃焼させる18種類の生薬を配合した「漢方薬」！</li>
+                    <li>「満量処方」で肥満症・便秘・むくみを改善！</li>
+                    <li>飲みやすい！（1回1包）</li>
+                    <li>メディアに多数掲載！芸能人も愛用！</li>
+                    <li>定期コースなら初回限定50％OFF＋送料無料！【回数縛りなし！】</li>
+                </ol>
+            </div>
+            <p>&nbsp;</p>
+
+        </div>
+
+        <div class="text-box">
+
+           <h3 class="midashi-h3-01">（１）脂肪を燃焼させる18種類の生薬を配合した「漢方薬」！</h3>
+            <img src="img/kiji/image5.jpg" alt="">
+
+            <p>脂肪がたまっていく原因を知っていますか・・・？</p>
+            <p>実は、<span class="rred">摂取した栄養物を消費できないこと</span>が脂肪がたまる原因の一つなんです。</p>
+            <p>『生漢煎「防風通聖散」』は、<span class="rred">摂取した栄養物を効率よく消費</span>することで脂肪を落としていきます！</p>
+            <p>そのカラクリは・・・</p>
+            <p>『生漢煎「防風通聖散」』に含まれる<span class="rred">「18種類の生薬」にあります！</span></p>
+            <p><span class="rred">「18種類の生薬」が、</span></p>
+            <p> <span class="bold k-bg-y">代謝UP→便通促進・清熱→利尿</span></p>
+            <p>このサイクルをつくり、<span class="rred">確実に脂肪を落としていきます。</span></p>
+            <p>&nbsp;</p>
+
+            <h3 class="midashi-h3-01">（２）「満量処方」で肥満症・便秘・むくみを改善！</h3>
+            <img src="img/kiji/image6.jpg" alt="">
+
+            <p>漢方薬の処方は、<span class="bold">1日分の生薬の配合バランスが決められているのをご存知ですか？</span></p>
+            <p>処方は大きくこちらに2つに分かれます。</p>
+            <p><span class="bold">・1日分使っている漢方薬・・・「満量処方」<br>・1日の半分の量を使っている漢方薬・・・「1/2処方」</span></p>
+            <p>「1/2処方」の漢方薬も数多くありますが、<br>『生漢煎「防風通聖散」』は、<span class="rred">1日分丸々使っている「満量処方（最大処方27.1g）」！！</span></p>
+            <p><span class="rred">満量処方の漢方薬だから、脂肪を内側から分解・促進・排出促進し肥満・便秘を改善</span>の効果効能が発揮されるんです！</p>
+            <p>また、<br>肥満症・便秘だけでなく、<span class="rred">”むくみ”改善にも効果があり、</span><br>むくんで太って見えるというお悩みの方にもピッタリな漢方薬でもあります♪</p>
+            <p>&nbsp;</p>
+
+            <h3 class="midashi-h3-01">（３）飲みやすい！（1回1包）</h3>
+            <img src="img/kiji/image10.jpg" alt="">
+
+            <p>1箱に入っているのは</p>
+            <p><span class="bold">90包（1包×3回×30日分）</span></p>
+            <p>1回分が1包になっているので、飲む量に迷わず飲めます。</p>
+            <p><span class="rred">スティックタイプで、持ち運びも便利なサイズ</span>なので、必要な分だけ持ち歩けるのもおすすめポイントの一つです♪</p>
+            <p>&nbsp;</p>
+
+            <h3 class="midashi-h3-01">（４）メディアに多数掲載！芸能人も愛用！</h3>
+            <img src="img/kiji/image7.png" alt="">
+
+            <p>女優の神田うのさん、井上和香さん、青木英李さんなど、<br>数多くの芸能人も愛飲して効果を実感！</p>
+            <p>TV、雑誌など多数メディアにも掲載されている今話題の漢方薬が『生漢煎「防風通聖散」』です♪</p>
+            <p>&nbsp;</p>
+
+            <h3 class="midashi-h3-01">（５）定期コースなら初回限定50％OFF＋送料無料！【回数縛りなし！】</h3>
+            <img src="img/kiji/image2.png" alt="">
+
+            <p>通常価格8,580円＋330円＝8,910円(税込)が</p>
+            <p>定期コースなら<span class="bold">毎回送料込(無料)で</span></p>
+            <p><span class="rred">初回→4,290円(税込)【50％OFF】<br>2回目以降→6,490円(税込)【27％OFF】</span></p>
+            <p>に✨</p>
+            <p><span class="bold">定期コースに縛りが無い</span>ので、効果を実感できなければ、1ヶ月目でも解約可能！</p>
+            <p>効果を実感したら、継続してお得に飲み続けることができます。</p>
+            <p>実際に、<span class="rred">99.2％の方が定期コース</span>を申し込んでいます✨</p>
+
+            <img src="img/kiji/image1.jpg" alt="">
+
+            <p>しかも、このページからの購入者限定で、<br>どのコースでも初回から使える<span class="rred">【1,100円OFF】クーポンプレゼント中</span>！</p>
+            <p>かなりお得です♪</p>
+
+        </div>
+
+        <div class="button">
+            <p class="center font13 bold">＼定期コースなら初回限定50％OFF＋送料無料／</p>
+            <a href="./links/bouhu.php<?php echo "?" . $query2; ?>" target="_blank">公式サイトでお得に購入する<i
+                    class="fas fa-arrow-circle-right"></i></a>
+        </div>
+
+        
+       　<h2 class="midashi-h2-03">『生漢煎「防風通聖散」』の口コミ</h2>
+       
+
+        <div class="text-box">
+            <p>『生漢煎「防風通聖散」』体験者のともちんさん（35歳・女性・事務）にインタビューをさせていただきました。</p>
+
+            <img src="img/kiji/image8.jpg" alt="">
+
+            <div class="boxDeco borderBox-green3 greenBox">
+                <h4>Q）『生漢煎「防風通聖散」』購入のキッカケは？？</h4>
+
+                <p>A）事務仕事でもともとあまり動くことが少なくなってしまったのですが、コロナもあって余計に動かなくなったり、ストレス解消のカラオケにも行く機会がめっきり減り、お腹まわりのお肉が増えていくのを見て見ぬ振りをしていました。
+                </p>
+                <p>ずっと座り続けているからか、足のむくみも毎日ひどくて、久々に会った友達から「ちょっと太った？」と言われたのがちょっとショックで・・・</p>
+                <p>いろいろなサプリを試したのですがあまり効果を実感できず。<br>効果効能が認められている医薬品レベルという生漢煎を見つけて試しに購入してみることにしました。</p>
+                <p>今までの経験上、1ヶ月以上は飲んだ方が確実に効くと思ったので、定期コースで購入しました。</p>
+            </div>
+
+            <div class="boxDeco borderBox-green3 greenBox">
+                <h4>Q）『生漢煎「防風通聖散」』の効果は？</h4>
+
+                <p>A）まず、便通がかなり良くなりました。<br>もともと悪い方ではなかったのですが、食前に生漢煎を飲むと食後にけっこうな確率で便が出るようになりました。これは飲み始めてすぐに実感しました。</p>
+                <p>2週間くらい飲み続けたところ、むくみ感が減ってきたように感じます。<br>1ヶ月後の変化としては、体重はちょっと減ったかなぁというくらいでしたが、体脂肪が27％→25％くらいに落ちました。</p>
+                <p>体脂肪が25％台になることがかなり久々だったので、これは生漢煎のおかげかと思っています。</p>
+            </div>
+
+            <div class="boxDeco borderBox-green3 greenBox">
+                <h4>Q）『生漢煎「防風通聖散」』を継続して飲んでる？</h4>
+
+                <p>はい！飲んでます。</p>
+                <p>持ち運びが簡単で、飲みやすく、外食のときもササッとすぐに飲めるので重宝しています。<br>漢方独特な苦味がありますが、良薬口に苦しということもあり、便通も良く効果も実感しているので、ありがたく飲んでます。<br>私の中では必須アイテムになっています。
+                </p>
+            </div>
+
+            <p class="right font13">※個人の感想です。</p>
+
+        </div>
+
+        <div class="text-box center font20">
+            <h4>その他にもたくさんのお声が！！」<br />＼『生漢煎「防風通聖散」』を<br />体験した皆さんの声／</h4>
+        </div>
+
+        <div class="text-box">
+        <div class="review-box boxDeco borderBox-pink3 pinkBox">
+            <div>
+                <h4 class="font16">すーさん・36歳・女性・専業主婦<br /><span class="pink bold">評価：非常に良い</span></h4>
+
+                <p>もともと便秘気味だったのですが、出産してからはさらに便秘がひどくなりました。1週間でないのは当たり前でお腹がとても張り苦しい毎日を過ごしていました。<br>たまたまサイトで見かけたこちらの商品をお試しで試してみたところ2、
+                    3日飲み始めてから快便が続き体も軽くなり体調が良くなったように思いました。<br>スティックタイプで持ち運びもとても便利で、外出時にも常に飲むようにしています。味も漢方特有の苦味とかはなく飲みやすいと思いました。
+                </p>
+                <p>お値段もリーズナブルでこれからも利用しようと思います。</p>
+                </div>
+            </div>
+
+            <div class="review-box boxDeco borderBox-pink3 pinkBox">
+                <div>
+                <h4 class="font16">ケイさん・40歳・女性・自営業<br /><span class="pink bold">評価：良い</span></h4>
+               
+                <p>生漢煎[防風通聖散]を購入したのは便秘と体重増加で悩んでいて、なんとかしたいと思って何か良いものがないか探していてたまたま見つけたのが生漢煎でした。</p>
+                <p>生漢煎を使って良かったと思ったのは、生漢煎のおかげでお通じが頻繁に来るようになったことです。<br>全く来ないわけではなかったのですが、日によってお通じがあったりなかったり安定しない感じでしたが、生漢煎を飲んでからはほぼ毎日、1日の中でも2回以上お通じが来ることも多くなりました。
+                </p>
+                <p>悪かったと思ったのは、効果がありすぎてなのか、お腹がかなり緩くなって下痢になりやすくなったことです。大事なときには逆に飲まない方が安心かもということもあり、飲むタイミングは調整しています。</p>
+                </div>
+            </div>
+
+            <div class="review-box boxDeco borderBox-pink3 pinkBox">
+                <div>
+                <h4 class="font16">コバヤシwebさん・29歳・女性・管理職<br /><span class="pink bold">評価：良い</span></h4>
+
+                <p>生漢煎を購入したきっかけは、20代後半から気になりだしたお腹のお肉です。食べれば食べるだけお腹にたまり、挙げ句の果てにはジーンズのずっしりと乗っかる始末、、。</p>
+                <p>生漢煎を飲んで良かったことは、2ヶ月で5キロ痩せたことです。<br>特にお腹周りと顔周りがスッキリしてとても満足しています。また他の漢方に比べて甘くて飲みやすいところも良いところです。</p>
+                <p>悪かったことは、値段が高いことです。数ヶ月続けないと効果が出ないと言われているのにこの値段はきついなと思いました。</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="button">
+            <p class="center font13 bold">＼定期コースなら初回限定50％OFF＋送料無料／</p>
+            <a href="./links/bouhu.php<?php echo "?" . $query2; ?>" target="_blank">公式サイトでお得に購入する<i
+                    class="fas fa-arrow-circle-right"></i></a>
+        </div>
+
+        
+
+        <div class="text-box">
+
+            <h2 class="midashi-h2-03">『生漢煎「防風通聖散」』は定期コースがお得！</h2>
+            <img src="img/kiji/image2.png" alt="">
+
+            <p>1箱通常価格8,580円＋330円＝8,910円(税込)が</p>
+            <p>定期コースなら<span class="bold">毎回送料込(無料)</span>で</p>
+            <p><span class="rred">初回→4,290円(税込)【50％OFF】<br>2回目以降→6,490円(税込)【27％OFF】</span></p>
+            <p>に✨</p>
+            <p><span class="bold">定期コースに縛りが無い</span>ので、効果を実感できなければ、1ヶ月目でも解約可能！</p>
+            <p>効果を実感したら、継続してお得に飲み続けることができます。</p>
+            <p>実際に、<span class="rred">99.2％の方が定期コース</span>を申し込んでいます✨</p>
+            <p>箱数によってさらにお得に！！</p>
+
+            <p class="center"><span class="kpink font24">↓↓↓↓↓↓</span></p>
+
+            <h3 class="midashi-h3-01">【6箱まとめて定期コース】</h3>
+            <img src="img/kiji/image11.png" alt="">
+
+            <p>6箱（6ヶ月分）がまとめて届きます♪</p>
+
+            <p>通常購入だと8,910円×6ヶ月分＝53,460円（税込）ですが、<br><span class="rred">6箱まとめて購入すると30,140円（税込・送料込・手数料込）</span>に！</p>
+            <p><span class="rred">23,320円もお得に買える</span>んです♪</p>
+            <p>さらに、<br><span class="rred">「ボタニカルホットジェル」2本</span>が付いてきます♪</p>
+            <p>1ヶ月あたり5,023円で購入できて、<br>1ヶ月1箱届く定期コースよりもお得な料金設定になっているので、<br>ずっと飲み続けたい方は6ヶ月の定期コースがおすすめです！</p>
+
+            <p>&nbsp;</p>
+
+            <h3 class="midashi-h3-01">【3箱まとめて定期コース】</h3>
+            <img src="img/kiji/image12.png" alt="">
+
+            <p>3箱（3ヶ月分）がまとめて届きます♪</p>
+            <p>&nbsp;</p>
+            <p><span class="rred">＋「ボタニカルホットジェル」1本が付いてきます♪</span></p>
+            <p>&nbsp;</p>
+            <p>こちらも通常価格で購入するより10,780円もお得になります♪</p>
+            <p>6ヶ月間はまだわからないけど、3ヶ月間は飲み続ける！<br>という方におすすめです！</p>
+            <p>すでに半年以上ずっと飲み続ける！と決めている方なら、<br>断然「6箱まとめて定期コース」がお得ですが、<br>まずは「1箱から定期コース」でお試しもおすすめです！</p>
+
+            <p>&nbsp;</p>
+
+            <img src="img/kiji/image1.jpg" alt="">
+            <p>しかも、このページからの購入者限定で、<br>
+                どのコースでも初回から使える<span class="rred">【1,100円OFF】クーポンプレゼント中！</span><br>
+                さらにお得に購入できちゃいます！</p>
+            <p>詳しくは公式サイトをご参考ください♪</p>
+        </div>
+
+        <div class="button">
+            <p class="center font13 bold">＼定期コースなら初回限定50％OFF＋送料無料／</p>
+            <a href="./links/bouhu.php<?php echo "?" . $query2; ?>" target="_blank">公式サイトでお得に購入する<i
+                    class="fas fa-arrow-circle-right"></i></a>
+        </div>
+
+    </section>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <?php include('footer.php'); ?>
+</div>
